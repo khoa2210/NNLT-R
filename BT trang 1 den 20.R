7dd074f2734999821c2e7011ffd12dae7408c54a
khoa2210 <73996278+khoa2210@users.noreply.github.com>
2021-05-09 20:47:15 +0700

    Add files via upload

---
+++ BT trang 1 den 20.R
@@ -0,0 +1,42 @@
+
+getwd()
+setwd("C:/Users")
+x <- rnorm(10)
+my.object <- rnorm(10)
+My.object.u <- 15
+ my.object.L <- 5
+ My.object.u + my.object.L
+help(lm)
+age <- c(50,62, 60,40,48,47,57,70,48,67)  insulin <- c(16.5,10.8,32.3,19.3,14.2,11.3,15.5,15.8,16.2,11.2)
+age <- c(50,62, 60,40,48,47,57,70,48,67) insulin <- c(16.5,10.8,32.3,19.3,14.2,11.3,15.5,15.8,16.2,11.2)
+age <- c(50,62, 60,40,48,47,57,70,48,67)
+insulin <- c(16.5,10.8,32.3,19.3,14.2,11.3,15.5,15.8,16.2,11.2)
+age <- c(50,62, 60,40,48,47,57,70,48,67)
+insulin <- c(16.5,10.8,32.3,19.3,14.2,11.3,15.5,15.8,16.2,11.2)
+My.object.u <- 15
+my.object.L <- 5
+My.object.u + my.object.L
+tuan <- data.frame(age, insulin)
+
+
+ins <- edit(data.frame())
+chol <- read.table("chol.txt", header=TRUE)
+x <- (1:12)
+x
+seq(12)
+x <- (12:5)
+seq(12,7)
+seq(4, 6, 0.25)
+seq(length=10, from=2, to=15)
+rep(10, 3)
+rep(c(1:4), 3)
+rep(c(1.2, 2.7, 4.8), 5)
+rep(c(1.2, 2.7, 4.8), 5)
+gl(2, 8)
+gl(3, 5)
+gl(2, 10, length=20)
+gl(2, 5, label=c("C", "T"))
+rep(1:4, c(2,2,2,2))
+x <- .leap.seconds[1:3]
+rep(x, 2)
+rep(as.POSIXlt(x), rep(2, 3))
